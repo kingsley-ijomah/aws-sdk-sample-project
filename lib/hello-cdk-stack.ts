4eaa5bb99f3683f89540d0acadbc83484e5ed3a8
Kingsley Ijomah <git-example@codehance>
2022-10-12 10:37:09 -0400

    Adding bucket

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -8,7 +10,14 @@ export class HelloCdkStack extends cdk.Stack {
 
     // The code that defines your stack goes here
     const queue = new sqs.Queue(this, 'HelloCdkQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+      visibilityTimeout: cdk.Duration.seconds(300),
+    });
+
+    // create new bucket
+    const bucket = new Bucket(this, 'HelloCdkBucket', {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
     });
   }
 }
